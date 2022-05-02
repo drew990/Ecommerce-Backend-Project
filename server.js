55bc864071cefb6343492d737dd0ff270b29989e
Andrew Banagas <andrewbanagas40@gmail.com>
2022-05-01 19:41:53 -0700

    Updated model folder with proper model sets

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const routes = require("./routes");
 // import sequelize connection
+const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -8,18 +9,13 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-//Connects to mysql Database
-// const db = mysql.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: process.env.mysqlPassword,
-//   database: "ecommerce_db",
-// });
-
+// turn on routes
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}!`);
+// turn on connection to db and server
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}!`);
+  });
 });
